6e9fe2437b2a2f16acd5dee8ffde53b93d9e20bb
pintubeservi-star <pintubeservi@gmail.com>
2026-04-05 17:04:03 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,14 @@
+export const metadata = {
+  title: 'Kolma RD | Supermercado Digital',
+  description: 'Delivery de colmado en Cotuí',
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body>{children}</body>
+      <body style={{ margin: 0, padding: 0, backgroundColor: '#000' }}>
+        {children}
+      </body>
     </html>
   )
 }
